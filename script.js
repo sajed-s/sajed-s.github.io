0984dbb123880eef385589b02c060bd765fdd0ac
Sajed Sarabandi <sajed.sarabandi@gmail.coml>
2025-07-16 11:46:27 +0200

    Update 3D sphere and add OrbitControls

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("mousemove", (e) => {
-  const x = (e.clientX / window.innerWidth) * 100;
-  const y = (e.clientY / window.innerHeight) * 100;
-  document.body.style.backgroundPosition = `${50 + (x - 50) / 10}% ${50 + (y - 50) / 10}%`;
-});
